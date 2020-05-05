21b65660870915545a26d2d2a68d41b00b69f796
Alan <Alan@192.168.109.20>
2020-05-05 15:40:32 -0400

    Initial commit

---
+++ first-app/src/main/java/edu/scranton/cs/first/app/FirstappApplication.java
@@ -0,0 +1,21 @@
+package edu.scranton.cs.first.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FirstappApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		  return application.sources(FirstappApplication.class);
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappApplication.class, args);
+	}
+
+}
